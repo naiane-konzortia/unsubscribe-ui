2598dc8c1c6f293994d95ddfb421832ca91b0a40
Naiane <naiane.negri@investhub.ventures>
2022-09-21 19:28:37 -0300

    fix: build errors

---
+++ build/precache-manifest.fbacb2236d1c7ad0150bbc6d7a998240.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "820bcfa3edbf3579fc55",
+    "url": "/static/css/main.e118589a.chunk.css"
+  },
+  {
+    "revision": "820bcfa3edbf3579fc55",
+    "url": "/static/js/main.6da5d478.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "807ea070eb46517638bf",
+    "url": "/static/js/2.f817d207.chunk.js"
+  },
+  {
+    "revision": "b07d3fcf09e500d72b36786de5912aab",
+    "url": "/static/media/Elevation.b07d3fcf.ttf"
+  },
+  {
+    "revision": "b30388b64dfeb135374486f0f0cfe126",
+    "url": "/static/media/noah-regular.b30388b6.ttf"
+  },
+  {
+    "revision": "bf0c05019cb5e9a1ad44bf4e3e75070d",
+    "url": "/static/media/vision-italic.bf0c0501.ttf"
+  },
+  {
+    "revision": "8e5c7ef2ae0f4e67c5f2224c2f715a14",
+    "url": "/static/media/Konzortia.8e5c7ef2.svg"
+  },
+  {
+    "revision": "c5c96fe22010aee381cffa73f181e0e1",
+    "url": "/index.html"
+  }
+];
